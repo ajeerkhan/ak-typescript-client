e50b60b8c1bc763773437853d2815836d957285e
ajeerkhan <ajeerkhan@NBC02WM49VHTD5.EMIRATES.COM>
2023-02-18 19:33:05 +0400

    cleaned up

--- src/client/geocoding-npm-client.ts
+++ src/client/geocoding-npm-client.ts
@@ -1,7 +1,16 @@
 import { geocoding } from "create-ak-typescript-library";
-import {Geocode, GeocodeCollection } from  "create-ak-typescript-library/dist-cjs/lib/geocoding/model";//"/dist-cjs/lib/geocoding/model";
+import { Options } from  "create-ak-typescript-library/dist-cjs/lib/geocoding/model";//"/dist-cjs/lib/geocoding/model";
+
 try {
-   geocoding.geocode("chennai").then((value)=>{
+   const options : Options = {
+      apiKey: '44bed54b990108565360d420f7ec3d8a',
+      apiUrl: '/geocode/v1',
+      baseUrl : 'https://api.7x.ax'
+   };
+   geocoding.geocode("chennai", options)
+   //geocoding.search("madurai", options)
+   //geocoding.reverse("9.122965","78.411352", options)
+   .then((value)=>{
       console.log('Geocodes', JSON.stringify(value));
    }, (reason:any)=>{
       console.log('reason Geocodes', JSON.stringify(reason));
@@ -9,30 +18,3 @@ try {
 } catch (error) {
   console.log('error', error);
 } 
-/* 
-try {
-   geocoding.search("madurai").then((value)=>{
-      console.log('Geocodes search', JSON.stringify(value));
-   }, (reason:any)=>{
-      console.log('reason search', JSON.stringify(reason));
-   });
-} catch (error) {
-  console.log('error', error); 
-} 
-
-try {
-   geocoding.reverse("9.122965","78.411352").then((value)=>{
-      console.log('Geocodes reverse', JSON.stringify(value));
-   }, (reason:any)=>{
-      console.log('reason reverse', JSON.stringify(reason));
-   });
-} catch (error) {
-  console.log('error', error); 
-}  */
-
-/*
-const  clientInvoke = async () => {
-   const  result =  await geocoding.geocode("chennai");
-   console.log(result);
-}
-*/
